8ce1934eeafff1f4cffa6d599ad2a05f20c93451
John Else <john.else@gmail.com>
2023-09-17 22:32:30 +0100

    Start to implement commandline argument parsing

--- bin/tabler.ml
+++ bin/tabler.ml
@@ -1,1 +1,24 @@
-let () = ()
+open Cmdliner
+
+let tabler samples waves =
+  Printf.eprintf "samples = %d\n" samples;
+  Printf.eprintf "waves = %d\n" waves
+
+let samples =
+  let doc = "Number of samples per waveform" in
+  Arg.(value & opt int 2048 & info ["samples"] ~docv:"SAMPLES" ~doc)
+
+let waves =
+  let doc = "Number of waveforms to generate" in
+  Arg.(value & opt int 256 & info ["waves"] ~docv:"WAVES" ~doc)
+
+let cmd =
+  let doc = "Generate a wavetable" in
+  let man = [
+  ] in
+  let info = Cmd.info "tabler" ~doc ~man in
+  let term = Term.(const tabler $ samples $ waves) in
+  Cmd.v info term
+
+let () =
+  exit (Cmd.eval cmd)
