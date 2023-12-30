7470d6d6be3c898a74b43e90b51b45b758ea1022
John Else <john.else@gmail.com>
2023-12-30 00:30:35 +0000

    Implement generator functions for standard waveforms

--- lib/generator.ml
+++ lib/generator.ml
@@ -18,9 +18,11 @@ let string_of_waveform = function
   | Square -> "square"
   | Triangle -> "triangle"
 
+type generator_fn = float -> float
+
 type generator =
   | Waveform of waveform
-  | Custom of (float -> float)
+  | Custom of generator_fn
 
 let generator_of_string str =
   match waveform_of_string str with
@@ -33,3 +35,32 @@ let generator_of_string str =
 let string_of_generator = function
   | Waveform waveform -> string_of_waveform waveform
   | Custom _ -> "custom"
+
+let pi_by_2 = Float.pi /. 2.0
+let three_pi_by_2 = 3. *. pi_by_2
+
+let saw x =
+  if x < Float.pi
+  then x /. Float.pi
+  else x /. Float.pi -. 2.
+
+let square x =
+  if x < Float.pi
+  then 1.
+  else -1.
+
+let triangle x =
+  if x < pi_by_2
+  then x /. pi_by_2
+  else begin
+    if x < three_pi_by_2
+    then 2.0 -. x /. pi_by_2
+    else x /. pi_by_2 -. 3.0
+  end
+
+let fn_of_generator = function
+  | Waveform Saw -> (fun x -> saw x)
+  | Waveform Sine -> (fun x -> sin x)
+  | Waveform Square -> (fun x -> square x)
+  | Waveform Triangle -> (fun x -> triangle x)
+  | Custom fn -> fn
--- lib/generator.mli
+++ lib/generator.mli
@@ -4,10 +4,14 @@ type waveform =
   | Square
   | Triangle
 
+type generator_fn = float -> float
+
 type generator =
   | Waveform of waveform
-  | Custom of (float -> float)
+  | Custom of generator_fn
 
 val generator_of_string : string -> generator option
 
 val string_of_generator : generator -> string
+
+val fn_of_generator : generator -> generator_fn
