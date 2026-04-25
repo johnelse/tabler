ed85222514626894c7a29e38328aaf76710e72d2
John Else <john.else@gmail.com>
2026-04-25 13:55:48 +0100

    Expand documentation

--- lib/expressions.mli
+++ lib/expressions.mli
@@ -1,4 +1,6 @@
-(** The type of generator functions. *)
+(** The type of generator functions. [position] is a normalised wave index,
+    i.e. the first wave in the table will be generated with position=0.0, and
+    the last wave in the table will be generated with position=1.0. *)
 type generator_fn = position:float -> theta:float -> float
 
 (** Load an function using the supplied expression, and return a float -> float
